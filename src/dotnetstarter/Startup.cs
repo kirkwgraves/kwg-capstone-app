c2af785daf6bf7da8ca8272a302d4fa5483db589
Kirk Walker Graves <graves.kirk@gmail.com>
2016-05-24 17:58:55 -0700

    Added Configuration logic to Startup.cs and injected dependencies for MVC and Mapper

--- src/dotnetstarter/Startup.cs
+++ src/dotnetstarter/Startup.cs
@@ -1,13 +1,52 @@
 ﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 public class Startup
 {
-	public void Configure(IApplicationBuilder app)
-	{
-		app.UseDefaultFiles();
-		app.UseStaticFiles();
+    public Startup(IHostingEnvironment env)
+    {
+        // Set up configuration sources.
+
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            .AddEnvironmentVariables();
+
+        if (env.IsDevelopment())
+        {
+            // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+            builder.AddUserSecrets();
+
+            // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+            builder.AddApplicationInsightsSettings(developerMode: true);
+        }
+
+        builder.AddEnvironmentVariables();
+        Configuration = builder.Build();
     }
+
+    public static IConfigurationRoot Configuration { get; set; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add framework services.
+        services.AddApplicationInsightsTelemetry(Configuration);
+
+
+        services.AddMvc()
+            .AddJsonOptions(opt =>
+            {
+                opt.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
+
+    }
+
 
     public static void Main(string[] args) => WebApplication.Run(args);
 }
