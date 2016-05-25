99280ca88ada1ca3c6ba00da5058c51275dd7d67
Kirk Walker Graves <graves.kirk@gmail.com>
2016-05-24 18:25:19 -0700

    Edited Startup to find bug

--- src/dotnetstarter/Startup.cs
+++ src/dotnetstarter/Startup.cs
@@ -8,43 +8,35 @@ using Newtonsoft.Json.Serialization;
 
 public class Startup
 {
-    public static IConfigurationRoot Configuration { get; set; }
+    //public static IConfigurationRoot Configuration { get; set; }
 
-    public Startup(IHostingEnvironment env)
-    {
-        // Set up configuration sources.
+    //public Startup(IHostingEnvironment env)
+    //{
+    //    // Set up configuration sources.
 
-        var builder = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json")
-            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-            .AddEnvironmentVariables();
+    //    var builder = new ConfigurationBuilder()
+    //        .AddJsonFile("appsettings.json")
+    //        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+    //        .AddEnvironmentVariables();
 
-        if (env.IsDevelopment())
-        {
-            // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
-            builder.AddUserSecrets();
+    //    if (env.IsDevelopment())
+    //    {
+    //        // For more details on using the user secret store see /*http://go.microsoft.com/fwlink/?LinkID=532709*/
+    //builder.AddUserSecrets();
 
-            // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
-            builder.AddApplicationInsightsSettings(developerMode: true);
-        }
+    //        // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+    //        builder.AddApplicationInsightsSettings(developerMode: true);
+    //    }
 
-        builder.AddEnvironmentVariables();
-        Configuration = builder.Build();
-    }
+    //    builder.AddEnvironmentVariables();
+    //    Configuration = builder.Build();
+    //}
 
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-        // Add framework services.
-        services.AddApplicationInsightsTelemetry(Configuration);
-
-
-        services.AddMvc()
-            .AddJsonOptions(opt =>
-            {
-                opt.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-            });
+        services.AddMvc();
 
     }
 
